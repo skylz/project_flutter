6998ca8974177cacc902890934fa74386f4e5097
Minki, Kim <xowhdltjd@gmail.com>
2020-10-27 17:05:49 +0900

    apply futureBuilder for ListView

--- weather_flutter_provider/lib/screen/searchLocationScreen.dart
+++ weather_flutter_provider/lib/screen/searchLocationScreen.dart
@@ -16,69 +16,73 @@ class _SearchLoactionScreenState extends State<SearchLoactionScreen> {
   // ListView 출력을 위한 filterLocation
   List<String> filterLocationList;
 
-  @override
-  void initState() {
-    provider.getLoadLocationData();
-    filterLocationList = provider.getLocationList();
-    super.initState();
+  Future loadFilterLocationList() async {
+    await provider.getLoadLocationData();
+    List<String> filterLocationList = provider.getLocationList();
+    return filterLocationList;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: TextField(
-                  controller: _controller,
-                  textAlign: TextAlign.left,
-                  textAlignVertical: TextAlignVertical.center,
-                  decoration: InputDecoration(
-                      hintText: '지역을 검색해주세요.',
-                      prefix: Padding(
-                        padding: EdgeInsets.fromLTRB(10, 0, 15, 0),
-                        child: Icon(Icons.search),
-                      ),
-                      suffix: IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: () {
-                          _controller.clear();
-                        },
-                      )),
-                  onChanged: (value) {
-                    setState(() {
-                      filterLocationList = provider
-                          .getLocationList()
-                          .where((location) => location.contains('$value'))
-                          .toList();
-                    });
-                  },
-                ),
+    print('build 실행');
+    return FutureBuilder(
+      future: loadFilterLocationList(),
+      builder: (context, snapshot) {
+        return Padding(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: SafeArea(
+            child: Scaffold(
+              backgroundColor: Colors.white,
+              body: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: TextField(
+                      controller: _controller,
+                      textAlign: TextAlign.left,
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                          hintText: '지역을 검색해주세요.',
+                          prefix: Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 15, 0),
+                            child: Icon(Icons.search),
+                          ),
+                          suffix: IconButton(
+                            icon: Icon(Icons.clear),
+                            onPressed: () {
+                              _controller.clear();
+                            },
+                          )),
+                      onChanged: (value) {
+                        setState(() {
+                          filterLocationList = provider
+                              .getLocationList()
+                              .where((location) => location.contains('$value'))
+                              .toList();
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount:
+                          (snapshot.data == null ? 0 : snapshot.data.length),
+                      itemBuilder: (BuildContext context, int index) {
+                        provider.getNameCode(index);
+                        return WeatherTile();
+                      },
+                    ),
+                  )
+                ],
               ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: ListView.builder(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: (filterLocationList == null
-                      ? 0
-                      : filterLocationList.length),
-                  itemBuilder: (BuildContext context, int index) {
-                    provider.getNameCode(index);
-                    return WeatherTile();
-                  },
-                ),
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
