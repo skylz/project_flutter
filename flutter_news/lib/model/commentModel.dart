de1672bba4797f761e0a6cf75b25b9982762e773
Minki, Kim <xowhdltjd@gmail.com>
2020-11-27 11:28:04 +0900

    add comment screen

---
+++ flutter_news/lib/model/commentModel.dart
@@ -0,0 +1,38 @@
+class Comment {
+  final String userName;
+  final String commentDate;
+  final String comment;
+
+  Comment({this.userName, this.commentDate, this.comment});
+}
+
+List<Comment> demoComments = [
+  Comment(
+      userName: 'gmlt****',
+      commentDate: '2020.11.25. 10:21:07',
+      comment: '정말 좋은 기사입니다. 이 기사에서 이 문단이 핵심인것 같네요!'),
+  Comment(
+      userName: 'hear****',
+      commentDate: '2020.11.25. 10:23:15',
+      comment: '이 문단은 살짝 편향적인 의견인것 같습니다.'),
+  Comment(
+      userName: 'kcch****',
+      commentDate: '2020.11.25. 10:25:52',
+      comment: '저는 살짝 사실이 아니라고 생각하는데, 다른 분들은 어떻게 생각하시나요?'),
+  Comment(
+      userName: 'rlax****',
+      commentDate: '2020.11.25. 10:27:11',
+      comment: '저는 그래도 어느정도 지지하는 편입니다.'),
+  Comment(
+      userName: 'whit****',
+      commentDate: '2020.11.25. 10:30:18',
+      comment: '저는 반대입니다. 그냥 느낌적으로 아니라는 생각이 드네요.'),
+  Comment(
+      userName: 'dhtn****',
+      commentDate: '2020.11.25. 10:31:22',
+      comment: '얼른 빨리 갈등이 봉합됐으면 하는 마음입니다.'),
+  Comment(
+      userName: 'imme****',
+      commentDate: '2020.11.25. 10:33:05',
+      comment: '빨리 이 사건이 해결되길 바랍니다.')
+];
--- flutter_news/lib/screen/commentScreen.dart
+++ flutter_news/lib/screen/commentScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/model/commentModel.dart';
 import 'package:get/get.dart';
 import '../controller/newsController.dart';
 import '../model/newsModel.dart';
@@ -15,6 +16,10 @@ class _CommentScreenState extends State<CommentScreen> {
   final News newsData = Get.arguments[1];
   final int index = Get.arguments[2];
   final int titleIndex = Get.arguments[3];
+
+  // 댓글을 처리하기 위함
+  final TextEditingController _commentController = TextEditingController();
+  String comment = '';
 
   @override
   Widget build(BuildContext context) {
@@ -45,159 +50,309 @@ class _CommentScreenState extends State<CommentScreen> {
               fontSize: 20),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 25, right: 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Text('${newsData.heading}',
-                style: Theme.of(context).textTheme.headline4.copyWith(
-                    color: Colors.black87,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                    // letterSpacing: 1.0,
-                    height: 1.3)),
-            SizedBox(
-              height: 5,
-            ),
-            Text('${newsData.author}',
-                style: Theme.of(context).textTheme.headline4.copyWith(
-                    color: Colors.black87,
-                    fontWeight: FontWeight.w300,
-                    fontSize: 14,
-                    // letterSpacing: 1.5,
-                    height: 1.3)),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  // color: Color(0xFFF3F5F7),
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15)),
-              child: Padding(
-                padding: const EdgeInsets.all(0.0),
-                child: Text('$textContent',
-                    style: Theme.of(context).textTheme.headline4.copyWith(
-                        color: Colors.black87,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                        letterSpacing: 1.5,
-                        height: 1.3)),
+      body: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 25, right: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Divider(
-              thickness: 0.5,
-              color: Colors.grey,
-            ),
-            IntrinsicHeight(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    child: Row(
-                      children: [
-                        InkWell(
-                          child: Icon(
-                            newsData.heartFill[index]
-                                ? Icons.favorite
-                                : Icons.favorite_border,
+              Text('${newsData.heading}',
+                  style: Theme.of(context).textTheme.headline4.copyWith(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                      // letterSpacing: 1.0,
+                      height: 1.3)),
+              SizedBox(
+                height: 5,
+              ),
+              Text('${newsData.author}',
+                  style: Theme.of(context).textTheme.headline4.copyWith(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 14,
+                      // letterSpacing: 1.5,
+                      height: 1.3)),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    // color: Color(0xFFF3F5F7),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Text('$textContent',
+                      style: Theme.of(context).textTheme.headline4.copyWith(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                          letterSpacing: 1.5,
+                          height: 1.3)),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Divider(
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          InkWell(
+                            child: Icon(
+                              newsData.heartFill[index]
+                                  ? Icons.favorite
+                                  : Icons.favorite_border,
+                              color: Colors.red,
+                              size: 30,
+                            ),
+                            onTap: () {
+                              // heart 채울지 말지 결정
+                              newsData.heartFill[index] =
+                                  newsData.heartState[index];
+
+                              if (newsData.heartFill[index]) {
+                                // heartCount 증가
+                                newsData.heartCount[index]++;
+
+                                // Histroy에 데이터 추가
+                                _newsController.newsHistory.add(NewsHistory(
+                                    heading: newsData.heading,
+                                    author: newsData.author,
+                                    historyContents: _newsController
+                                        .newsListData[titleIndex]
+                                        .content[index],
+                                    routeIndex: titleIndex));
+                              } else {
+                                // heartCount 감소
+                                newsData.heartCount[index]--;
+
+                                // Histroy에 데이터 제거
+                                _newsController.newsHistory.removeWhere(
+                                    (item) =>
+                                        item.historyContents ==
+                                        _newsController.newsListData[titleIndex]
+                                            .content[index]);
+                              }
+
+                              setState(() {
+                                newsData.heartState[index] =
+                                    !newsData.heartState[index];
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('${newsData.heartCount[index]}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline4
+                                  .copyWith(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 18,
+                                      letterSpacing: 1.5,
+                                      height: 1.3)),
+                        ],
+                      ),
+                    ),
+                    VerticalDivider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      width: 20,
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.chat_outlined,
                             color: Colors.red,
                             size: 30,
                           ),
-                          onTap: () {
-                            // heart 채울지 말지 결정
-                            newsData.heartFill[index] =
-                                newsData.heartState[index];
-
-                            if (newsData.heartFill[index]) {
-                              // heartCount 증가
-                              newsData.heartCount[index]++;
-
-                              // Histroy에 데이터 추가
-                              _newsController.newsHistory.add(NewsHistory(
-                                  heading: newsData.heading,
-                                  author: newsData.author,
-                                  historyContents: _newsController
-                                      .newsListData[titleIndex].content[index],
-                                  routeIndex: titleIndex));
-                            } else {
-                              // heartCount 감소
-                              newsData.heartCount[index]--;
-
-                              // Histroy에 데이터 제거
-                              _newsController.newsHistory.removeWhere((item) =>
-                                  item.historyContents ==
-                                  _newsController
-                                      .newsListData[titleIndex].content[index]);
-                            }
-
-                            setState(() {
-                              newsData.heartState[index] =
-                                  !newsData.heartState[index];
-                            });
-                          },
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text('${newsData.heartCount[index]}',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline4
-                                .copyWith(
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('${demoComments.length}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline4
+                                  .copyWith(
                                     color: Colors.red,
                                     fontWeight: FontWeight.w400,
-                                    fontSize: 18,
-                                    letterSpacing: 1.5,
-                                    height: 1.3)),
-                      ],
+                                    fontSize: 16,
+                                    // letterSpacing: 1.5,
+                                  ))
+                        ],
+                      ),
                     ),
-                  ),
-                  VerticalDivider(
-                    color: Colors.grey,
-                    thickness: 0.5,
-                    width: 20,
-                  ),
-                  Container(
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.ios_share,
-                          color: Colors.red,
-                          size: 30,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text('공유',
-                            style:
-                                Theme.of(context).textTheme.headline4.copyWith(
-                                      color: Colors.red,
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 16,
-                                      // letterSpacing: 1.5,
-                                    ))
-                      ],
+                    VerticalDivider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      width: 20,
                     ),
-                  )
-                  // 공유
-                ],
+                    // 공유
+                    Container(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.ios_share,
+                            color: Colors.red,
+                            size: 30,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('공유',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline4
+                                  .copyWith(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 16,
+                                    // letterSpacing: 1.5,
+                                  ))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Divider(
-              thickness: 0.5,
-              color: Colors.grey,
-            ),
-          ],
+              Divider(
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      reverse: false,
+                      shrinkWrap: true,
+                      itemCount: demoComments.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 5),
+                          child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              elevation: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(0.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 4),
+                                      child: Text(
+                                          '${demoComments[demoComments.length - index - 1].userName}',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline4
+                                              .copyWith(
+                                                  color: Colors.black87,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 16)),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 10),
+                                      child: Text(
+                                          '${demoComments[demoComments.length - index - 1].commentDate}',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline4
+                                              .copyWith(
+                                                  color: Colors.black87,
+                                                  fontWeight: FontWeight.w300,
+                                                  fontSize: 12)),
+                                    ),
+                                    Text(
+                                        '${demoComments[demoComments.length - index - 1].comment}',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w400,
+                                        )),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Divider(
+                                      thickness: 0.2,
+                                      color: Colors.black,
+                                    )
+                                  ],
+                                ),
+                              )),
+                        );
+                      }))
+            ],
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.only(left: 25, right: 25, bottom: 30),
+          child: Container(
+            alignment: Alignment(0.0, 1.0),
+            child: TextFormField(
+              controller: _commentController,
+              onChanged: (value) {
+                setState(() {
+                  comment = value;
+                });
+              },
+              decoration: InputDecoration(
+                  fillColor: Color(0xFFF3F5F7),
+                  hintText: '해당 기사의 문단에 의견을 남겨주세요.',
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(16),
+                  prefixIcon: Icon(
+                    Icons.account_circle_outlined,
+                    color: Colors.black38,
+                  ),
+                  suffix: InkWell(
+                    child: Text('댓글',
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                              color:
+                                  comment != '' ? Colors.red : Colors.black38,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              // letterSpacing: 1.5,
+                            )),
+                    onTap: () {
+                      if (comment != '') {
+                        setState(() {
+                          demoComments.add(Comment(
+                              userName: 'now****',
+                              commentDate: '2020.11.25. 10:35:11',
+                              comment: comment));
+                        });
+                      }
+                    },
+                  )),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
